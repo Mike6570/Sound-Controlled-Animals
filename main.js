d9a0fccdea9cbcab2f6ff8f233138387de6d4b85
Mike6570 <83513986+Mike6570@users.noreply.github.com>
2022-01-30 10:51:23 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,33 @@
 function startClassification()
 {
     navigator.mediaDevices.getUserMedia({audio:true});
-    classifier=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/LM46PQxJt/',modelReady);
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/LM46PQxJt/model.json',modelReady);
 }
 function modelReady(){
     classifier.classify(gotResults);
+}
+
+function gotResults(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        random_number_r=Math.floor(Math.random()*255) + 1;
+        random_number_g=Math.floor(Math.random()*255) + 1;
+        random_number_b=Math.floor(Math.random()*255) + 1;
+
+        document.getElementById("result_label").innerHTML= 'Detected Sound - '+results[0].label;
+        document.getElementById("result_label").style.color="rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
+        img = document.getElementById('animal_image');
+
+        if (results[0].label == "Barking") {
+            img.src='bark.gif';
+        } else if (results[0].label == "Meowing") {
+            img.src='meow.gif';
+        } else{
+            img.src='listen.gif';
+        }
+
+    }
+
 }
